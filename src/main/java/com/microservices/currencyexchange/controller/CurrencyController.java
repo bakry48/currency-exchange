4622e7b941fe6462b57553d1ea7ebb531f0ca8af
DESKTOP-0OSS82D\Ahmed <abyounes@banquemisr.com>
2023-04-03 12:08:58 +0200

    configure the zipkin distributed tracing to tracing all request

--- src/main/java/com/microservices/currencyexchange/controller/CurrencyController.java
+++ src/main/java/com/microservices/currencyexchange/controller/CurrencyController.java
@@ -2,6 +2,8 @@ package com.microservices.currencyexchange.controller;
 
 import com.microservices.currencyexchange.models.entity.CurrencyExchange;
 import com.microservices.currencyexchange.services.CurrencyExchangeService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +16,7 @@ import java.math.BigDecimal;
 public class CurrencyController {
     private final Environment environment;
     private final CurrencyExchangeService currencyExchangeService;
+    private Logger logger = LoggerFactory.getLogger(CurrencyController.class);
     public CurrencyController(Environment environment , CurrencyExchangeService currencyExchangeService) {
         this.environment = environment;
         this.currencyExchangeService= currencyExchangeService;
@@ -24,6 +27,8 @@ public class CurrencyController {
      @PathVariable String from,
      @PathVariable String to
     ){
+        // INFO [currency-exchange,e900dacfaf40c17d5a1d38c79a600954,194c3bd19349678b] 15136 --- [nio-8000-exec-1] c.m.c.controller.CurrencyController      : retrieveExchangeValue from EGP to USD
+        logger.info("retrieveExchangeValue from {} to {}" , from , to);
         CurrencyExchange currencyExchange = new CurrencyExchange(1001L, from, to, BigDecimal.valueOf(50));
         String serverPort =environment.getProperty("local.server.port");
         currencyExchange.setEnvironment(serverPort);
